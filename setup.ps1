88dac5d6defb81ea8bc4f32f0dd8498a6c6195fc
Hydradevx <themaskedprogrammer1234@gmail.com>
2025-02-02 13:59:03 +0530

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -3,7 +3,7 @@ Write-Host "You Must have Git and NodeJS installed to run this script"
 
 # Clone the repository
 Write-Host "Cloning the repository..."
-git clone https://github.com/Hydrion-Tools/Hydrion-S3LFB0T.git
+git clone https://github.com/Hydradevx/Hydrion-S3LFB0T.git
 
 # Change directory
 Set-Location -Path "Hydrion-S3LFB0T"
