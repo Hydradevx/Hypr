442b8cab57d1336411404a828b6843be1da73429
HydraDevX <themaskedprogrammer1234@gmail.com>
2025-07-13 15:25:27 +0530

    Minor config.json import fix

--- build/bot/managers/autoReactState.js
+++ build/bot/managers/autoReactState.js
@@ -1,7 +1,10 @@
 import fs from "fs"
 import path from "path"
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 let config;
-const configPath = path.join(__dirname, "../config.json");
+const configPath = path.join(__dirname, "../../../config.json");
 config = JSON.parse(fs.readFileSync(configPath, "utf-8"));
 const autoReactState = {
     active: true,
--- src/bot/managers/autoReactState.ts
+++ src/bot/managers/autoReactState.ts
@@ -1,6 +1,11 @@
 import fs from "fs"
 import path from "path"
 
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 let config: any;
 const configPath = path.join(__dirname, "../config.json");
 
